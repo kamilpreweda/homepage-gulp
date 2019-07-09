23bb2e5db1f09b0bc827e8e1bd7760e48d4341ac
Kamil Preweda <preweda.kamil@o2.pl>
2019-07-09 15:52:04 +0200

    feat: new building system

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,10 @@
 import moment from 'moment';
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+const navigationSwitcher = document.querySelector('.navigation__switcher--js');
+
+navigationSwitcher.addEventListener('click', (e) => {
+    const navigation = document.querySelector('.navigation__js');
+    navigation.classList.toggle('navigation__visible');
+});
